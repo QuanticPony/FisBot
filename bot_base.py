f61074417476a583cbe3451aeb47bde60b5bb4d9
Unai Lería Fortea <67756626+QuanticPony@users.noreply.github.com>
2020-07-09 17:40:55 +0200

    Responder a comandos basicos

--- bot_base.py
+++ bot_base.py
@@ -4,74 +4,54 @@ import discord
 
 from discord.ext import commands
 
-#bot = commands.Bot(command_prefix=commands.when_mentioned_or('.'), help_command=".Help")
-#client = discord.Client()
 bot = commands.Bot(command_prefix=commands.when_mentioned_or("."))
+
+msg = discord.User
+
+def is_me(msg):
+    return msg.author == bot.user
+
+def is_command(msg):
+    if msg.content.startswith(".ntc"):
+        return True
+
+    return False
+    
 
 @bot.event
 async def on_ready():
-    print("Listo jefe!")
-    await bot.change_presence(status=discord.Status.online, activity=discord.Game(name=".Prueba"))
-
+    await bot.change_presence(status=discord.Status.online, activity=discord.Game(name=".help"))
 
 @bot.event
-async def on_message(msg_in):
-    if msg_in.content.startswith(".Prueba"):
-        channel = msg_in.channel
-        await channel.send("Prueba")
+async def on_message(msg):
+    if msg.content.startswith(".help"):
+        channel = msg.channel
+        await channel.send(
+            '''```css
+Commandos disponibles:
+    .help   
+    .prueba
+    .ntc "name" (new text channel at user's menssage category)   
+    .nvc "name" (new voice channel at current user category)   
+            ```''')
 
-    if msg_in.content.startswith(".Prueba2"):
-        channel = msg_in.channel
-        await channel.send("Prueba2")
+    if msg.content.startswith(".prueba"):
+        channel = msg.channel
+        await channel.send("prueba")
 
+    if msg.content.startswith(".ntc"):
+        prefix, name = msg.content.split()
+        category = await msg.channel.category
+        await category.create_text_channel(name=name)
 
+    if msg.content.startswith(".nvc"):
+        prefix, name = msg.content.split()
+        channel = msg.author.voice.channel
+        await channel.category.create_voice_channel(name=name)
 
-## Para debug
-#import logging
-#logging.basicConfig(level=logging.INFO)
-#
-#
-#    dictionary = {}
-#    user_names = open("user_names.txt","r")
-#    line = user_names.readline()
-#    while line:
-#        line = line.split(" ",2)
-#        user_id = int(line[0])
-#        user_index = int(line[1])   #TODO: debug esa parte
-#        dictionary.setdefault(user_id, user_index)
-#
-#        line = user_names.readline()
-#    user_names.close
-#
-#
-#    user_dat = open("user_dat.dat","r+")
-#    user_dat.se
-#    user_dat.close
-#
-#
-#
-#
-#    from discord.ext import commands
-#    descripcion = '''Bot en proceso de mejora, tratar con cuidado'''
-#    bot = commands.Bot(command_prefix=commands.when_mentioned_or('.'), description=descripcion)
-#    bot.game(".Help")
-#
-#    msg = discord.Message
-#    msg.channel = 0
-#
-#    @bot.event
-#    async def on_ready():
-#        print('{0.user} ha llegado!!'.format(bot))
-#
-#    @bot.event
-#    async def on_message(message):
-#        if message.author == bot.user:
-#            return
-#
-#        if message.content == ".Prueba":
-#            msg.c
+    if msg.content.startswith(""):
+        pass
 
-        
 
 
 token_file = open("token.txt", "r")
