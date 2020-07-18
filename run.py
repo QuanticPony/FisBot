25f004c4e2b912ef69066b52dc08e474b2ac8a42
Unai Lería Fortea <67756626+QuanticPony@users.noreply.github.com>
2020-07-18 23:48:24 +0200

    Corregir nombre

--- run.py
+++ run.py
@@ -1,10 +1,8 @@
 import discord
 import asyncio
 from bot_class import FisBot
-from user_class import FisUser
 
 bot = FisBot(command_prefix='.')
 
-# Token read and run bot
 with open("token.txt", "r") as token_file:
     bot.run(str(token_file.read()))
