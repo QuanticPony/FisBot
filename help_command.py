2a45d7f024f46f3e5557035078af3e92a897e320
Unai Lería Fortea <67756626+QuanticPony@users.noreply.github.com>
2020-07-31 19:58:04 +0200

    Añadir .ayuda

--- help_command.py
+++ help_command.py
@@ -12,10 +12,53 @@ class help_command(
         self.bot = bot
         self.bot.remove_command('help')
 
+    @commands.command(
+        name='ayuda',
+        brief='''Muestra informacion general del servidor''',
+        description='''Te da informacion basica sobre el funcionamiento del servidor. Para informaicon de comandos y categorias prueba ```.help [command|category]```''',
+        usage='.ayuda'
+    )
+    @commands.has_permissions(embed_links=True)
+    async def _ayuda(self, ctx):
+        ayuda=discord.Embed(
+            title='Hola {.nick}:'.format(ctx.author),
+            description='Vemos que has pedido ayuda en {0.channel.mention} sobre el servidor {0.guild.name}:'.format(ctx),
+            color=discord.Color.gold()
+        )
+        ayuda.add_field(
+            name='Normas basicas:',
+            value='''1. Ser respetuoso
+            2. Tener como nickname del servidor vuestro nombre real (al menos un apellido)''',
+            inline=False
+        )
+        ayuda.add_field(
+            name='Estructura del servidor:',
+            value='''Este servidor esta estructurado en **categorias** de *dudas*, *trabajando*, *no trabajando*, *general* y algun *grupo de trabajo*.
+            El nombre de las categorias ya explica de que va el tema de cada canal de texto o voz dentro de la misma. La categoria mas interesante es *general*:''',
+            inline=False
+        )
+        
+        for category in ctx.guild.categories:
+             if category.name=='General':
+                break
+
+        for channel in category.text_channels:
+            ayuda.add_field(
+                name='**General**.*{.name}*:'.format(channel),
+                value='''{0.mention}:
+                {0.topic}'''.format(channel),
+                inline=False
+            )
+
+        if not ctx.author.dm_channel:
+            await ctx.author.create_dm()
+        return await ctx.author.dm_channel.send(embed=ayuda)
+
+
 
     @commands.command(
         name='help',
-        aliases=['ayuda','h'],
+        aliases=['h'],
         help='''¿Dudas sobre como poner musica? .help Musica
         ¿Duda sobre el comando .play? .help play''',
         brief='''Muestra informacion de categorias y comandos''',
@@ -25,7 +68,7 @@ class help_command(
         Y si, tenemos tanto tiempo que hemos completado el comando .help help''',
         usage='.help [category|command]'
     )
-    @commands.has_permissions(add_reactions=True,embed_links=True)
+    @commands.has_permissions(embed_links=True)
     async def _help(self, ctx, *nombre):
     # No se da ni categoria ni comando 
         if not nombre:  
