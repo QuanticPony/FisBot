c7e1b70817c072dc4625936f0dc262503a298d58
Unai Lería Fortea <67756626+QuanticPony@users.noreply.github.com>
2020-08-03 00:20:09 +0200

    Añadir custom_roles.py

---
+++ custom_roles.py
@@ -0,0 +1,45 @@
+
+class custom_roles(
+    commands.Cog,
+    name='Roles'
+    ):
+
+    def __init__(self, bot):
+        self.bot = bot
+
+
+    def _roles_help_embed(self, ctx):
+        ayuda=discord.Embed(
+            title='Roles:',
+            description='Estos son los roles disponibles por subida de nivel:',
+            color=discord.Color.purple()
+        )
+        nivel = 0
+        for rol in ctx.guild.roles:
+            if not rol.permissions.manage_roles:
+                
+                ayuda.add_field(
+                    name='{.mention}'.format(rol),
+                    value='Desbloqueado al nivel {0}'.format(nivel),
+                    inline=False
+                )
+
+
+    @commands.group(
+        pass_context=True, 
+        name='rol',
+        aliases=['roles'],
+        help='''
+        
+        ''',
+        brief='''Opera conjuntos de comandos''',
+        description='''Permite modificar los roles existentes: crear, borrar, renombrar y actualizarlos''',
+        usage='.rol <subcommand>'
+    )
+    async def _roles(self, context):
+        if context.invoked_subcommand is None:
+            pass
+
+
+    
+      
