62b5c953c795cb074a4a7e15f22e943f71096fdd
Unai Lería Fortea <67756626+QuanticPony@users.noreply.github.com>
2020-08-03 23:14:02 +0200

    Añadir 2 grupos

--- custom_roles.py
+++ custom_roles.py
@@ -1,3 +1,5 @@
+import discord
+from discord.ext import commands
 
 class custom_roles(
     commands.Cog,
@@ -8,7 +10,7 @@ class custom_roles(
         self.bot = bot
 
 
-    def _roles_help_embed(self, ctx):
+    def __help_embed(self, ctx) -> discord.Embed:
         ayuda=discord.Embed(
             title='Roles:',
             description='Estos son los roles disponibles por subida de nivel:',
@@ -17,12 +19,14 @@ class custom_roles(
         nivel = 0
         for rol in ctx.guild.roles:
             if not rol.permissions.manage_roles:
-                
                 ayuda.add_field(
                     name='{.mention}'.format(rol),
                     value='Desbloqueado al nivel {0}'.format(nivel),
                     inline=False
                 )
+                nivel += 1
+        return ayuda
+        
 
 
     @commands.group(
@@ -38,7 +42,28 @@ class custom_roles(
     )
     async def _roles(self, context):
         if context.invoked_subcommand is None:
-            pass
+            await context.send(embed=custom_roles._help_embed(context))
+
+    @_roles.group(
+        pass_context=True,
+        name='create',
+        aliases=['c'],
+        brief='Crea un nuevo rol con permisos personalizados',
+        description='Crea un rol ',
+        help='''C''',
+    )
+    async def _create(self, ctx):
+        if ctx.invoked_subcommand is None:
+            await ctx.send('Debes especificar el tipo de permisos, si quieres consultarlos prueba: ```.help rol create```')
+
+    @_roles._create.command(
+        pass_context=True,
+        name='general',
+        aliases=['g'],
+        brief='Crea un rol'
+    )
+    async def _general(self, ctx, *, name):
+        discord.Permissions
 
 
     
