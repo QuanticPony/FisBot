93eeebbd3cc6a9d31391c82089aaf89e826c1c68
Unai Lería Fortea <67756626+QuanticPony@users.noreply.github.com>
2020-08-14 13:00:12 +0200

    Añadir interfaz base de datos usuarios

---
+++ database/users.py
@@ -0,0 +1,14 @@
+from ..classes.user_class import FisUser
+
+class UsersDB():
+    def add_user(self, user: FisUser) -> bool:
+        pass
+
+    def del_user(self, user: FisUser) -> bool:
+        pass
+
+    def get_user(self, user: FisUser) -> FisUser:
+        pass
+
+    def list_users(self) -> tuple: # Ordenados alfabeticamente
+        pass
