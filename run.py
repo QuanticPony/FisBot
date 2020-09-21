399d5f2d3ad284dcdf6c1f45be1c142c4c81be34
Aitor Lería <aitorleria@gmail.com>
2020-09-21 20:05:43 +0100

    Modificar command_prefix

--- run.py
+++ run.py
@@ -3,9 +3,9 @@ import asyncio
 import fisbot
 from fisbot import FisBot, FisUser, UsersDB
 
-BOT_PATH = ''#'/home/pi/Bots/Fisbot/FisBot/'
+BOT_PATH = '/home/pi/Bots/Fisbot/FisBot/'
 
-bot = FisBot(command_prefix='-', path=BOT_PATH)
+bot = FisBot(command_prefix='.', path=BOT_PATH)
 
 with open(BOT_PATH + "token.txt", "r") as token_file:
     bot.run(str(token_file.read()))
